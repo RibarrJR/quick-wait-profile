3e7f28f2b7b2685c3051a27b9b7ba4c36f40c384
AdrianoValadaresFrwk <adrianovaladares@frwk.com.br>
2022-01-26 17:51:38 -0300

    Configuração para criar pequenos parcels

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { mountRootParcel } from 'single-spa';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'quick-wait-profile';
+  title = 'Quick Wait Profile';
+  componentMountRootParcel = mountRootParcel;
+  async getInforParcel() {
+    // @ts-ignore
+    return window.System.import('@frwk/frwkInfo');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { ParcelModule } from 'single-spa-angular/parcel';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TestComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TestComponent],
+  imports: [BrowserModule, AppRoutingModule, ParcelModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
